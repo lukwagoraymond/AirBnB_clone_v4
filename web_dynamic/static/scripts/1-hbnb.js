36d5d85b7b33de93b454e1cceed77f359bfb34f8
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-02-23 20:15:57 +0300

    Compiled Tasks 2 to Task 3

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,18 +2,17 @@ $(document).ready(() => {
   const idList = [];
   $('.amenity-list input:checkbox').change((e) => {
     const inputElement = $(e.target)
-    const amenityElem = $('.amenity-list li').has(inputElement);
-    const amenityName = amenityElem.text();
     if ( inputElement.prop('checked')) {
       idList.push(inputElement.data('id'));
       const h4Tag = $('DIV.amenities h4');
-      h4Tag.append(amenityName + ' ');
+      h4Tag.append(inputElement.data('name') + ' ');
     } else {
       const index = idList.indexOf(inputElement.data('id'));
       if (index > -1) {
 	idList.splice(index, 1);
       }
-      $('DIV.amenities h4').html($('DIV.amenities h4').html().replace(String(amenityName), ''));
+      $('DIV.amenities h4').html($('DIV.amenities h4').html().
+      replace(String(inputElement.data('name')), ''));
     }
   });  
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -4,6 +4,22 @@
 */
 
 $(document).ready(function () {
+    const idList = [];
+    $('.amenity-list input:checkbox').change((e) => {
+        const inputElement = $(e.target)
+        if ( inputElement.prop('checked')) {
+            idList.push(inputElement.data('id'));
+            const h4Tag = $('DIV.amenities h4');
+            h4Tag.append(inputElement.data('name')+ ' ');
+        } else {
+        const index = idList.indexOf(inputElement.data('id'));
+        if (index > -1) {
+        idList.splice(index, 1);
+        }
+        $('DIV.amenities h4').html($('DIV.amenities h4').html().replace(inputElement.data('name'), ''));
+        }
+    });
+
     const url = 'http://0.0.0.0:5001/api/v1/status/';
     $.get(url, function(data, responseStatus) {
         if (data.status === 'OK') {
