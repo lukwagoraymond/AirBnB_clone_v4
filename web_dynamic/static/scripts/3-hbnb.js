e0941ede799faa65c166c48715541509bc4ea4c9
iakev <kirimikmwongera@gmil.com>
2023-02-23 18:16:18 +0000

    modified task 2 , 3 and 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -6,15 +6,15 @@
 $(document).ready(function () {
     const idList = [];
     $('.amenity-list input:checkbox').change((e) => {
-        const inputElement = $(e.target)
-        if ( inputElement.prop('checked')) {
-            idList.push(inputElement.data('id'));
-            const h4Tag = $('DIV.amenities h4');
-            h4Tag.append(inputElement.data('name')+ ' ');
-        } else {
+      const inputElement = $(e.target)
+      if ( inputElement.prop('checked')) {
+        idList.push(inputElement.data('id'));
+        const h4Tag = $('DIV.amenities h4');
+        h4Tag.append(inputElement.data('name')+ ' ');
+      } else {
         const index = idList.indexOf(inputElement.data('id'));
         if (index > -1) {
-        idList.splice(index, 1);
+         idList.splice(index, 1);
         }
         $('DIV.amenities h4').html($('DIV.amenities h4').html().replace(inputElement.data('name'), ''));
         }
